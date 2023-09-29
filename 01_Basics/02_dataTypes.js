432472a889ef978fd73a11f85619146dbafa6d38
Rahul462000 <Rahul2018ME015abesit@gmail.com>
2023-09-29 20:28:13 +0530

    dataTypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all Js code as newer version
+console.log(3 + 3);
+console.log("singh");
+
+// string dataType
+let name = "hitesh";
+// integer dataType
+let age = 18;
+// boolean dataType
+let isLoggedIn = false;
+
+// when value is not given then it's dataTypt is undefined
+let state;
+console.log(typeof state);
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+console.log(typeof undefined); // undefined
+// null is a primitive datatype
+console.log(typeof null); // object
