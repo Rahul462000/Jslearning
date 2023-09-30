dc7c10d97e90c664e2c0dcc938542cea82e719ca
Rahul462000 <Rahul2018ME015abesit@gmail.com>
2023-09-30 11:21:16 +0530

    comparision

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,31 @@
+// comparisions are used to determine the correct value from given two or three values
+
+// 1.greater than
+// console.log(2 > 1);
+
+// 2.greater than or equal
+// console.log(2 >= 1);
+
+// 3.less than
+// console.log(2 < 1);
+
+//4. equivalence to rhs
+// console.log(2 == 1); // false
+
+// 5.not equal to rhs
+console.log(2 != 1);
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === this comparision with dataType
+
+console.log("2" === 2);
