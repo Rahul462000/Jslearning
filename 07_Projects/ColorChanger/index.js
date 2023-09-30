3e7874e7dc947b76437daaced8740ace969d23e7
Rahul462000 <Rahul2018ME015abesit@gmail.com>
2023-09-30 14:57:55 +0530

    ColorChangeProject

--- 07_Projects/ColorChanger/index.js
+++ 07_Projects/ColorChanger/index.js
@@ -1,3 +1,4 @@
+console.log("singh");
 // when a colored box is clicked the background is changed
 
 // 1.selecting buttons
@@ -5,10 +6,23 @@ const buttons = document.querySelectorAll(".button");
 // selecting body
 const body = document.querySelector("body");
 
-buttons.forEach(function (btn) {
-  console.log(btn);
-  btn.addEventListener("click", function (e) {
+buttons.forEach(function (button) {
+  console.log(button);
+  button.addEventListener("click", function (e) {
     console.log(e);
     console.log(e.target);
+    // taking the first color if grey is clicked with id
+    if (e.target.id === "grey") {
+      // it will change the background colour
+      body.style.backgroundColor = e.target.id;
+    } else if (e.target.id === "white") {
+      body.style.backgroundColor = e.target.id;
+    } else if (e.target.id === "blue") {
+      body.style.backgroundColor = e.target.id;
+    } else if (e.target.id === "yellow") {
+      body.style.backgroundColor = e.target.id;
+    } else if (e.target.id === "purple") {
+      body.style.backgroundColor = e.target.id;
+    }
   });
 });
--- 07_Projects/projectsset.md
+++ 07_Projects/projectsset.md
@@ -8,4 +8,31 @@
 
 ```javascript
 console.log("Rahul");
+// when a colored box is clicked the background is changed
+
+// 1.selecting buttons
+const buttons = document.querySelectorAll(".button");
+// selecting body
+const body = document.querySelector("body");
+
+buttons.forEach(function (button) {
+  console.log(button);
+  button.addEventListener("click", function (e) {
+    console.log(e);
+    console.log(e.target);
+    // taking the first color if grey is clicked with id
+    if (e.target.id === "grey") {
+      // it will change the background colour
+      body.style.backgroundColor = e.target.id;
+    } else if (e.target.id === "white") {
+      body.style.backgroundColor = e.target.id;
+    } else if (e.target.id === "blue") {
+      body.style.backgroundColor = e.target.id;
+    } else if (e.target.id === "yellow") {
+      body.style.backgroundColor = e.target.id;
+    } else if (e.target.id === "purple") {
+      body.style.backgroundColor = e.target.id;
+    }
+  });
+});
 ```
