73eb34eddbf32d2d092a6f3fd528112c7c4ecc67
Rahul462000 <Rahul2018ME015abesit@gmail.com>
2023-09-30 20:31:28 +0530

    h2 msg changed(final)

--- 07_Projects/ColorChanger/index.js
+++ 07_Projects/ColorChanger/index.js
@@ -7,6 +7,7 @@ const buttons = document.querySelectorAll(".button");
 const body = document.querySelector("body");
 const bodyText = body.querySelector("h2");
 console.log(bodyText);
+const colors = ["grey", "white", "Aquamarine", "DarkGoldenRod", "B5CB99"];
 buttons.forEach(function (button) {
   console.log(button);
   button.addEventListener("click", function (e) {
@@ -18,20 +19,29 @@ buttons.forEach(function (button) {
       body.style.backgroundColor = e.target.id;
       // below we are targetting the h2 tag and changing its color
       bodyText.style.color = "white";
+      bodyText.innerHTML = "grey is clicked";
     } else if (e.target.id === "white") {
       body.style.backgroundColor = e.target.id;
+      // below we are targetting the h2 tag and changing its color
       bodyText.style.color = "grey";
+      bodyText.innerHTML = "white is clicked";
     } else if (e.target.id === "Aquamarine") {
       body.style.backgroundColor = e.target.id;
-      bodyText.style.color = "purple";
+      // below we are targetting the h2 tag and changing its color
+      bodyText.style.color = "red";
+      bodyText.innerHTML = "Aquamarine is clicked";
     } else if (e.target.id === "DarkGoldenRod") {
       body.style.backgroundColor = e.target.id;
+      // below we are targetting the h2 tag and changing its color
       bodyText.style.color = "blue";
+      bodyText.innerHTML = "DarkGoldenRod is clicked";
 
       // here in this method can also change the color like given below method
     } else if (e.target.id === "B5CB99") {
       body.style.backgroundColor = "#B5CB99";
+      // below we are targetting the h2 tag and changing its color
       bodyText.style.color = "orange";
+      bodyText.innerHTML = " B5CB99 is clicked";
     }
   });
 });
