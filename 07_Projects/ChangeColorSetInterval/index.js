9238813c017ba01c072e01afd27f3204fcd93b97
Rahul462000 <Rahul2018ME015abesit@gmail.com>
2023-10-05 18:26:05 +0530

    random color with setInterval

---
+++ 07_Projects/ChangeColorSetInterval/index.js
@@ -0,0 +1,13 @@
+// generate a random color
+
+const randomcolor = function () {
+  // hex values in colors
+  const hex = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+
+console.log(randomcolor());
